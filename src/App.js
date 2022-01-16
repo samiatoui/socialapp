b02423aa9f114d47ea214eb52f9759a1c9cf1f21
Sami <atoui79@gmail.com>
2022-01-16 00:47:30 -0600

    progress

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import 'firebase/compat/firestore';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
+
 firebase.initializeApp({
   apiKey: "AIzaSyDifGkBVHxcQ_CKvfDwNt2YudhezViwt-Q",
   authDomain: "socialapp-10b8a.firebaseapp.com",
@@ -16,6 +17,7 @@ firebase.initializeApp({
   messagingSenderId: "520886012282",
   appId: "1:520886012282:web:ecf937c68f7559eb022ec3"
 })
+
 
 const auth = firebase.auth();
 const firestore = firebase.firestore();
@@ -26,10 +28,14 @@ function App() {
   return (
     <div className='App'>
       <header>
-        <h1>SocialApp</h1> <SignOut />
+        <div className='title'>SocialApp</div>
+        <div ><SignOut /></div>
       </header>
+      <div className='profile'>
+        {user ? <Profile /> : <SignIn />}
+      </div>
       <section>
-        {user ? <ChatRoom /> : <SignIn />}
+        {user ? <TimeLine /> : <SignIn />}
       </section>
     </div>
   );
@@ -50,17 +56,16 @@ function SignIn() {
 
 function SignOut() {
   return auth.currentUser && (
-    <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
+    <a className="sign-out" onClick={() => auth.signOut()}>Sign Out</a>
   )
 }
 
-function ChatRoom() {
+function TimeLine() {
   const dummy = useRef();
   const messagesRef = firestore.collection('messages');
   const query = messagesRef.orderBy('createdAt', 'desc').limit(100);
 
   const [messages] = useCollectionData(query, { idField: 'id' });
-
   const [formValue, setFormValue] = useState('');
 
 
@@ -78,40 +83,62 @@ function ChatRoom() {
     })
 
     setFormValue('');
+
   }
 
   return (<>
     <main>
-
       {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
-
       <span ref={dummy}></span>
-
     </main>
 
     <form onSubmit={sendMessage}>
-
       <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="What is on your mind?" />
-
-      <button type="submit" disabled={!formValue}>📩</button>
-
+      <button type="submit" disabled={!formValue}>Send</button>
     </form>
   </>)
 }
+
 function ChatMessage(props) {
-  const { text, uid, photoURL, displayName } = props.message;
+  const { text, uid, photoURL, displayName, createdAt } = props.message;
 
   const messageClass = uid == auth.currentUser.uid ? 'sent' : 'received';
-
   return (<>
     <div className={`message ${messageClass}`}>
-      <div className="userInfo">      <img src={photoURL} />
-      <h3>{displayName}</h3></div>
-
+      <div className="userInfo"> <img src={photoURL} />
+        <h3>{displayName}</h3></div>
       <p> {text} </p>
+
     </div>
   </>)
 }
 
+function Profile() {
+  let user = firebase.auth().currentUser;
+  var signupDate = new Date(user.metadata.creationTime);
 
+  const monthNames = ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+  ];
+
+  var month = monthNames[signupDate.getUTCMonth()];
+  var year = signupDate.getUTCFullYear();
+  var newdate = month + ", " + year;
+
+  return (<>
+    <div className='container'>
+      <div className='profile'>
+        <div className='profile-img'>
+          <img src={user.photoURL} />
+        </div>
+        <div className='profile-name'>
+          {user.displayName}
+        </div>
+        <div className='joined'>
+          📅  Joined {newdate}
+        </div>
+      </div>
+    </div>
+  </>)
+}
 export default App;
